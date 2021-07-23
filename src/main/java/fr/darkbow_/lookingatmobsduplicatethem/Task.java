76b358b8357e7c713240af289443ac7cbf50114d
BuildTools <unconfigured@null.spigotmc.org>
2021-07-23 19:28:32 +0200

    V1.2

--- src/main/java/fr/darkbow_/lookingatmobsduplicatethem/Task.java
+++ src/main/java/fr/darkbow_/lookingatmobsduplicatethem/Task.java
@@ -26,42 +26,44 @@ public class Task extends BukkitRunnable {
         }
 
         for(Player pls : Bukkit.getOnlinePlayers()){
-            Location eye = pls.getEyeLocation();
-            for(Entity e : pls.getNearbyEntities(30, 30, 30)){
-                if(e instanceof LivingEntity && !(e instanceof EnderDragon) && !(e instanceof Player)){
-                    if(!main.getEtimers().containsKey(pls)){
-                        Map<LivingEntity, Integer> emap = new HashMap<>();
-                        main.getEtimers().put(pls, emap);
-                    }
-
-                    LivingEntity entity = (LivingEntity) e;
-
-                    boolean copy = false;
-
-                    if(main.getEtimers().get(pls).containsKey(entity)){
-                        if(main.getEtimers().get(pls).get(entity) > 0){
-                            main.getEtimers().get(pls).put(entity, main.getEtimers().get(pls).get(entity) - 1);
-                        } else {
-                            main.getEtimers().get(pls).put(entity, main.getDelay());
-                            copy = true;
+            if(main.getConfig().getBoolean("gamemodes." + pls.getGameMode().toString().toLowerCase())){
+                Location eye = pls.getEyeLocation();
+                for(Entity e : pls.getNearbyEntities(30, 30, 30)){
+                    if(e instanceof LivingEntity && !(e instanceof EnderDragon) && !(e instanceof Player)){
+                        if(!main.getEtimers().containsKey(pls)){
+                            Map<LivingEntity, Integer> emap = new HashMap<>();
+                            main.getEtimers().put(pls, emap);
                         }
-                    } else {
-                        copy = true;
-                        main.getEtimers().get(pls).put(entity, main.getDelay());
-                    }
 
-                    if(copy){
-                        Vector toEntity = entity.getEyeLocation().toVector().subtract(eye.toVector());
-                        double dot = toEntity.normalize().dot(eye.getDirection());
+                        LivingEntity entity = (LivingEntity) e;
 
-                        List<Block> blocks = main.getLineOfSight(main.getHashSet(), 30, 1, pls);
-                        Block targetblock = blocks.get(0);
+                        boolean copy = false;
 
-                        if(dot > 0.99 && pls.getEyeLocation().distance(entity.getLocation()) <= pls.getEyeLocation().distance(targetblock.getLocation())){
-                            Entity clone = main.cloneEntity(entity);
-                            if(clone instanceof LivingEntity){
-                                LivingEntity livingClone = (LivingEntity) clone;
-                                main.getEtimers().get(pls).put(livingClone, main.getDelay());
+                        if(main.getEtimers().get(pls).containsKey(entity)){
+                            if(main.getEtimers().get(pls).get(entity) > 0){
+                                main.getEtimers().get(pls).put(entity, main.getEtimers().get(pls).get(entity) - 1);
+                            } else {
+                                main.getEtimers().get(pls).put(entity, main.getDelay());
+                                copy = true;
+                            }
+                        } else {
+                            copy = true;
+                            main.getEtimers().get(pls).put(entity, main.getDelay());
+                        }
+
+                        if(copy){
+                            Vector toEntity = entity.getEyeLocation().toVector().subtract(eye.toVector());
+                            double dot = toEntity.normalize().dot(eye.getDirection());
+
+                            List<Block> blocks = main.getLineOfSight(main.getHashSet(), 30, 1, pls);
+                            Block targetblock = blocks.get(0);
+
+                            if(dot > 0.99 && pls.getEyeLocation().distance(entity.getLocation()) <= pls.getEyeLocation().distance(targetblock.getLocation())){
+                                Entity clone = main.cloneEntity(entity);
+                                if(clone instanceof LivingEntity){
+                                    LivingEntity livingClone = (LivingEntity) clone;
+                                    main.getEtimers().get(pls).put(livingClone, main.getDelay());
+                                }
                             }
                         }
                     }
